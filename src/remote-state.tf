92ed54cc427de2dd9d8590036127d8237bc6d343
Igor Rodionov <goruha@gmail.com>
2025-02-20 05:01:07 +0300

    Added tests (#15)
    * Added tests
    
    * Added tests
    
    * Added tests
    
    * Added tests
    
    * Fix tests
    
    * Fix go sum
    
    * Fix tests
    
    * Pin version

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "1.5.0"
+  version = "1.8.0"
 
   component = var.vpc_component_name
 
@@ -9,7 +9,7 @@ module "vpc" {
 
 module "dns_delegated" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "1.5.0"
+  version = "1.8.0"
 
   component   = var.dns_delegated_component_name
   environment = var.dns_delegated_environment_name
---
+++ test/component_test.go
@@ -0,0 +1,103 @@
+package test
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/kafka"
+	"github.com/cloudposse/test-helpers/pkg/atmos"
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
+	awshelper "github.com/cloudposse/test-helpers/pkg/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type ComponentSuite struct {
+	helper.TestSuite
+}
+
+func (s *ComponentSuite) TestBasic() {
+	const component = "msk/basic"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	suffix := strings.ToLower(random.UniqueId())
+	inputs := map[string]interface{}{
+		"name": "msk-" + suffix,
+	}
+
+	defer s.DestroyAtmosComponent(s.T(), component, stack, &inputs)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, &inputs)
+	assert.NotNil(s.T(), options)
+
+	clusterName := atmos.Output(s.T(), options, "cluster_name")
+	assert.NotEmpty(s.T(), clusterName)
+
+	clusterArn := atmos.Output(s.T(), options, "cluster_arn")
+	assert.Contains(s.T(), clusterArn, clusterName)
+
+	storageMode := atmos.Output(s.T(), options, "storage_mode")
+	assert.Equal(s.T(), "LOCAL", storageMode)
+
+	bootstrapBrokers := atmos.Output(s.T(), options, "bootstrap_brokers")
+	bootstrapBrokersList := strings.Split(bootstrapBrokers, ",")
+	assert.Equal(s.T(), 2, len(bootstrapBrokersList))
+
+	bootstrapBrokersTLS := atmos.Output(s.T(), options, "bootstrap_brokers_tls")
+	bootstrapBrokersTLSList := strings.Split(bootstrapBrokersTLS, ",")
+	assert.Equal(s.T(), 2, len(bootstrapBrokersTLSList))
+
+	bootstrapBrokersPublicTLS := atmos.Output(s.T(), options, "bootstrap_brokers_public_tls")
+	assert.Empty(s.T(), bootstrapBrokersPublicTLS)
+
+	// Additional assertions for SASL and Zookeeper
+	// ...
+
+	client := awshelper.NewMSKClient(s.T(), awsRegion)
+	describeClusterOutput, err := client.DescribeCluster(context.Background(), &kafka.DescribeClusterInput{
+		ClusterArn: &clusterArn,
+	})
+	require.NoError(s.T(), err)
+	cluster := describeClusterOutput.ClusterInfo
+	assert.EqualValues(s.T(), "ACTIVE", cluster.State)
+
+	assert.EqualValues(s.T(), clusterName, *cluster.ClusterName)
+	assert.EqualValues(s.T(), clusterArn, *cluster.ClusterArn)
+	assert.EqualValues(s.T(), storageMode, cluster.StorageMode)
+
+	s.DriftTest(component, stack, &inputs)
+}
+
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "msk/disabled"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	suffix := strings.ToLower(random.UniqueId())
+	inputs := map[string]interface{}{
+		"name": "msk-" + suffix,
+	}
+
+	s.VerifyEnabledFlag(component, stack, &inputs)
+}
+
+func TestRunSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+
+	suite.AddDependency(t, "vpc", "default-test", nil)
+
+	subdomain := strings.ToLower(random.UniqueId())
+	inputs := map[string]interface{}{
+		"zone_config": []map[string]interface{}{
+			{
+				"subdomain": subdomain,
+				"zone_name": "components.cptest.test-automation.app",
+			},
+		},
+	}
+	suite.AddDependency(t, "dns-delegated", "default-test", &inputs)
+	helper.Run(t, suite)
+}
+
